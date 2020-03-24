1bb705998e4c6feb3ac6786ca10ce720568fcb30
Julio Rocha <julhaorocha@gmail.com>
2020-03-24 17:43:40 -0300

    Revert "Estado e Cidade"
    This reverts commit ca50d31a12b32dbd1a3a1cfedbb637c1cccd86f8.

--- src/main/java/com/juliorocha/cursomc/CursomcApplication.java
+++ src/main/java/com/juliorocha/cursomc/CursomcApplication.java
@@ -8,12 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.juliorocha.cursomc.domain.Categoria;
-import com.juliorocha.cursomc.domain.Cidade;
-import com.juliorocha.cursomc.domain.Estado;
 import com.juliorocha.cursomc.domain.Produto;
 import com.juliorocha.cursomc.repositories.CategoriaRepository;
-import com.juliorocha.cursomc.repositories.CidadeRepository;
-import com.juliorocha.cursomc.repositories.EstadoRepository;
 import com.juliorocha.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -24,12 +20,6 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
-	@Autowired
-	private EstadoRepository estadoRepository;
-	
-	@Autowired
-	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -55,18 +45,6 @@ public class CursomcApplication implements CommandLineRunner{
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
-		Estado est1 = new Estado(null, "Minas Gerais");
-		Estado est2 = new Estado(null, "São Paulo");
-		
-		Cidade c1 = new Cidade(null, "Uberlândia", est1);
-		Cidade c2 = new Cidade(null, "Campinas", est2);
-		Cidade c3 = new Cidade(null, "São Paulo", est2);
-		
-		est1.getCidades().addAll(Arrays.asList(c1));
-		est2.getCidades().addAll(Arrays.asList(c2, c3));
-		
-		estadoRepository.saveAll(Arrays.asList(est1, est2));
-		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		
 	}
--- src/main/java/com/juliorocha/cursomc/repositories/EstadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.juliorocha.cursomc.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.juliorocha.cursomc.domain.Estado;
-
-@Repository
-public interface EstadoRepository extends JpaRepository<Estado, Integer>{}
