02ac37bb9b1ea113ace74213b57da709c6ed8459
Julio Rocha <julhaorocha@gmail.com>
2020-03-24 17:38:19 -0300

    Testando

---
+++ src/main/java/com/juliorocha/cursomc/Teste.java
@@ -0,0 +1,10 @@
+package com.juliorocha.cursomc;
+
+import org.springframework.boot.SpringApplication;
+
+public class Teste {
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
