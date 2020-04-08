6469ff234d596f02fa3dc051426ad11e609e16d8
Julio Rocha <julhaorocha@gmail.com>
2020-04-08 20:42:03 -0300

    Validacao Sintatica com Bean Validation

--- src/main/java/com/juliorocha/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/juliorocha/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.juliorocha.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.juliorocha.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
@@ -9,6 +13,10 @@ public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="Otamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/juliorocha/cursomc/services/CategoriaService.java
+++ src/main/java/com/juliorocha/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.juliorocha.cursomc.domain.Categoria;
+import com.juliorocha.cursomc.dto.CategoriaDTO;
 import com.juliorocha.cursomc.repositories.CategoriaRepository;
 import com.juliorocha.cursomc.services.exceptions.DataIntegrityException;
 import com.juliorocha.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
 				orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
